92ba6686a934fc07fe69c829dc94f2907c73ad9b
Ethan Welsh <welshej@gmail.com>
2016-01-29 17:42:26 -0500

    skeleton for database interaction

--- src/Database.java
+++ src/Database.java
@@ -1,7 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class Database
 {
+    private String usersDB;
+    private String groupDB;
+
+    public Database(String usersPath, String groupPath)
+    {
+        this.usersDB = usersPath;
+        this.groupDB = groupPath;
+    }
+
+    public HashMap<String, User> UserDB() throws FileNotFoundException, ParseException
+    {
+        Scanner dbScanner = new Scanner(new File(this.usersDB));
+        fileJson = JSONParser().parse(dbScanner.read());
+        System.out.println(fileJson);
+
+    }
+
+    public HashMap<String, Group> GroupDB()
+    {
+
+        return null;
+    }
+
+
     public void addGroup()
     {
 
@@ -57,6 +88,10 @@ public class Database
         return null;
     }
 
+    public static void main(String args[])
+    {
+        UserDB()
+    }
 }
 
 class User
